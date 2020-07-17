a0a49672fecbcaedcb5eca4500324fafa26a413b
Dax <daaawx@gmail.com>
2020-07-17 23:52:53 +0200

    Set up consumers + routing

--- test_django_channels/routing.py
+++ test_django_channels/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import test_django_channels.chat.routing as chat_routing
 
 application = ProtocolTypeRouter({
-
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat_routing.websocket_urlpatterns
+        )
+    ),
 })
